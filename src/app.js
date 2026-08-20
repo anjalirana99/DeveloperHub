4e27c809b24f880b9a60498baba30520b0a09727
anjalirana99 <anjali.rana021@gmail.com>
2026-08-20 09:53:52 +0530

    Create Cluster + connecting to DB before Listening on APP via Mongoose

--- src/app.js
+++ src/app.js
@@ -1,51 +1,17 @@
 const express = require("express")
+const {connectCluster} = require("./config/database")
 const app = express()
-const {adminAuth} = require("./middleware/auth")
-
-//Multiple routeHandler for one route 
-app.get("/user",(req,res,next)=>{
-    console.log("Hi from1")
-        res.send("User FOund1")
-            next();
 
 
-})
-app.get("/user",(req,res,next)=>{
-    console.log("hi from2")
-})
-
-//MiddleWare for Admin Authorization
-app.use("/admin",adminAuth)
-
-app.use("/admin/getData",(req,res,next)=>{
-    res.send("Data collected")
-})
-app.use("/admin/DeleteUser",(req,res,next)=>{
-    res.send("User is Deleted Succesfully!!!")
-})
-
-
-// Error Handling
-// 1- uding try catch block in handler 
-//2- using wildcard handler with err 
-
-app.get("/user/login",(req,res)=>{
-    try{
-        throw new Error()
-        res.send("Loggein Succesfully!!!")
-    }
-    catch(err){
-        res.status(500).send("Error in Login Service....")
-    }
-})
-
-app.use("/",(err,req,res,next)=>{
-    if(err){
-        res.status(500).send("Something Went Wrong")
-    }
-})
-
-
-app.listen(7777,()=>{
+connectCluster()
+.then(()=>{
+    console.log("DB Connected Succesfully!!!")   // first connect to DB then listen on server require is async but connectdb was async
+    app.listen(7777,()=>{
     console.log("Server Started Succesfully at Port 7777.....")
 })
+})
+.catch((err)=>{
+    console.log("Error in DB Connection...")
+})
+
+
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+
+const connectCluster = async()=>{
+    await mongoose.connect("mongodb+srv://nodeProject_db_user:kIZGcRvHy3OBoaWU@nodeproject.jdef6oa.mongodb.net/")
+}
+
+module.exports = {connectCluster}
