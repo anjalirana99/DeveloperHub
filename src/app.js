6e5f6e0d7b973f8920842b2e4a8ca96c03fd7807
anjalirana99 <anjali.rana021@gmail.com>
2026-08-21 12:15:44 +0530

    Read data - Feed  API -get All User from DataBase

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const {userModel} = require("./models/user")
 
 app.use(express.json()) //middle ware to convert the json to JS object in incoming request for all routes
 
+
+// store user data dynamically 
 app.post("/signup", async(req,res)=>{
     const userInfo = req.body
     console.log(userInfo)
@@ -21,11 +23,49 @@ app.post("/signup", async(req,res)=>{
         res.send("User Stored")
         }
         catch(err){
-            console.log(err)
-            res.status(500).send(err)
+            res.status(500).send("Something Went Wrong")
         }
         
 })
+
+//get one user by email
+app.get("/user",async (req,res)=>{
+
+    try{
+        const userEmail = req.query.email
+        const user = await userModel.find({email:userEmail})
+
+        if(user.length !== 0){
+            res.send("User Found" + user)
+        }
+        else{
+            res.status(404).send("User Not Found!!")
+        }
+    }
+    catch(err){
+        res.status(500).send("Something Went Wrong")
+    }
+    
+    
+})
+
+// Feed API - GEt /feed - get All User from DataBase
+
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await userModel.find({})
+        if(users.length !==0){
+            res.send(users)
+        }
+        else{
+            res.status(404).send("No Users Found!!")
+        }
+    }
+    catch(err){
+        res.status(500).send("Something Went Wrong")
+    }
+})
+
 
 
 connectCluster()
